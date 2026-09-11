4251abc72f5a75c587d84d2ae24aeb10699ecdf0
Mathias Wagner Nielsen <mani1019@stud.ek.dk>
2026-09-11 14:10:47 +0200

    Do assignment 2

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -2,4 +2,10 @@ public class Book {
     private String title;
     private String author;
     private int publicationYear;
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,3 @@
 void main() {
-    
+    Book book = new Book("The Hobbit", "J.R.R. Tolkien", 1937);
 }
