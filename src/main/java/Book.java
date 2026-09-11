24bf1335835776cdb5b4f7be1944b9a750558ebc
Mathias Wagner Nielsen <mani1019@stud.ek.dk>
2026-09-11 14:20:24 +0200

    Do assignment 7

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -5,6 +5,18 @@ public class Book {
 
     private boolean read = false;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
     public void markAsRead() {
         read = true;
     }
