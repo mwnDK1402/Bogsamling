dad7f665df0e9648c09753eba9a5824fdb1192d6
Mathias Wagner Nielsen <mani1019@stud.ek.dk>
2026-09-11 21:26:53 +0200

    Target JDK 21

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,43 @@
-void main() {
-    Book[] books = {
-            new Book("The Hobbit", "J.R.R. Tolkien", 1937),
-            new Book("Harry Potter og De Vises Sten", "J.K. Rowling", 1997),
-            new Book("1984", "George Orwell", 1949),
-    };
+public final class Main {
+    public static void main(String[] args) {
+        Book[] books = {
+                new Book("The Hobbit", "J.R.R. Tolkien", 1937),
+                new Book("Harry Potter og De Vises Sten", "J.K. Rowling", 1997),
+                new Book("1984", "George Orwell", 1949),
+        };
 
-    books[0].markAsUnread(); // should be unread
-    books[1].markAsRead();   // should be read
-    books[2].markAsRead();
-    books[2].markAsUnread(); // should be unread
+        books[0].markAsUnread(); // should be unread
+        books[1].markAsRead();   // should be read
+        books[2].markAsRead();
+        books[2].markAsUnread(); // should be unread
 
-    Book classicBook = new Book("Harry Potter and the Half-Blood Prince",
-            "J.K. Rowling", 2005);
-    Book nonClassicBook = new Book("The God Delusion: A Study of Religious Belief and Skepticism",
-            "Richard Dawkins", 2006);
+        Book classicBook = new Book("Harry Potter and the Half-Blood Prince",
+                "J.K. Rowling", 2005);
+        Book nonClassicBook = new Book("The God Delusion: A Study of Religious Belief and Skepticism",
+                "Richard Dawkins", 2006);
 
-    System.out.printf("'%s' is %s%n", classicBook.getTitle(), classicBook.isClassic(2026) ? "a classic" : "not a classic");
-    System.out.printf("'%s' is %s%n", nonClassicBook.getTitle(), nonClassicBook.isClassic(2026) ? "a classic" : "not a classic");
+        System.out.printf("'%s' is %s%n", classicBook.getTitle(), classicBook.isClassic(2026) ? "a classic" : "not a classic");
+        System.out.printf("'%s' is %s%n", nonClassicBook.getTitle(), nonClassicBook.isClassic(2026) ? "a classic" : "not a classic");
 
-    Library library = new Library("Min bogsamling");
-    library.addBooks(books);
-    library.addBooks(classicBook, nonClassicBook);
+        Library library = new Library("Min bogsamling");
+        library.addBooks(books);
+        library.addBooks(classicBook, nonClassicBook);
 
-    System.out.printf("%nAntal bøger: %d%n%n", library.getNumberOfBooks());
-    library.printBooks();
+        System.out.printf("%nAntal bøger: %d%n%n", library.getNumberOfBooks());
+        library.printBooks();
 
-    int barWidth = 20;
-    System.out.printf("%s%n%n", "-".repeat(barWidth));
+        int barWidth = 20;
+        System.out.printf("%s%n%n", "-".repeat(barWidth));
 
-    Book foundBook = library.findBookByTitle("the hobbit");
+        Book foundBook = library.findBookByTitle("the hobbit");
 
-    if (foundBook != null) {
-        foundBook.printInfo();
-    } else {
-        System.out.println("Bogen blev ikke fundet.");
+        if (foundBook != null) {
+            foundBook.printInfo();
+        } else {
+            System.out.println("Bogen blev ikke fundet.");
+        }
+
+        System.out.printf("%n%s%n%n", "-".repeat(barWidth));
+        library.printUnreadBooks();
     }
-
-    System.out.printf("%n%s%n%n", "-".repeat(barWidth));
-    library.printUnreadBooks();
 }
